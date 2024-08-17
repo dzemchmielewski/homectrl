94bfcff16e3c9fbdd59e1c371ec0975103524a16
Pawel Chmielewski <chmielu@ais.pl>
2024-08-17 22:00:11 +0200

    Kitchen real-life implementation

--- board/kitchen_worker.py
+++ board/kitchen_worker.py
@@ -7,11 +7,12 @@ from ld2410 import LD2410
 from modules.darkness import DarknessSensor
 from modules.dht import DHTSensor
 from modules.pin_io import PinIO
-from common.common import CommonSerial
+from common.common import CommonSerial, time_ms
 
 
 class KitchenWorker(Worker):
-    TARGET_STATE = [".", "M", "S", "B"]
+
+    MAX_FLOATING_TIME = 20 * 1_000
 
     def __init__(self, debug=True):
         super().__init__("kitchen", debug)
@@ -21,22 +22,20 @@ class KitchenWorker(Worker):
         self.darkness_sensor = DarknessSensor("darkness", 9)
         self.human_presence = PinIO("human", 10)
         self.light_switch = PinIO("light", 0)
-        self.light_switch.set_signal(0)
-        self.previous_values = {
-            "darkness": None,
-            "presence": None,
-            "target_state": None
-        }
+        self.light_switch.set_signal(False)
 
         uart = CommonSerial(1, baudrate=256000, bits=8, parity=None, stop=1, tx=7, rx=6, timeout=1)
         self.radar = LD2410("LD2410", uart, debug=False)
 
         self.mqtt = None
 
-    def resolve_target_state(self, state):
-        if state < 0 or state > 3:
-            return "?"
-        return self.TARGET_STATE[state]
+    def handle_exception(self, exception):
+        traceback = uio.StringIO()
+        sys.print_exception(exception, traceback)
+        worker_data = self.get_data()
+        worker_data.error = traceback.getvalue()
+        worker_data.is_alive = False
+        worker_data.go_exit = True
 
     def start(self):
         self.log("START")
@@ -49,11 +48,17 @@ class KitchenWorker(Worker):
             self.mqtt = MQTTPublisher(self.name)
             worker_data.mqtt_connected = self.mqtt.connected
         except BaseException as e:
-            traceback = uio.StringIO()
-            sys.print_exception(e, traceback)
-            worker_data.error = traceback.getvalue()
-            worker_data.is_alive = False
-            worker_data.go_exit = True
+            self.handle_exception(e)
+
+        is_light_on: bool = False
+        floating_time = None
+        # TODO: previous values - send data, only
+        #  when those values changed
+        # previous_values = {
+        #     "darkness": None,
+        #     "presence": None,
+        #     "light": None
+        # }
 
         while not worker_data.go_exit:
 
@@ -66,17 +71,45 @@ class KitchenWorker(Worker):
                 #     worker_data.data["dht_time"] = time.localtime()
                 #     self.last_dht_read = time_ms()
 
-                # Darkness sensor:
-                worker_data.data["darkness"] = self.darkness_sensor.is_darkness()
-
                 # Human radar data:
                 data = self.radar.get_radar_data()
                 while data[0][0] not in range(0, 4):
                     data = self.radar.get_radar_data()
 
                 # Human detection:
-                worker_data.data["presence"] = self.human_presence.get_signal()
+                presence = self.human_presence.get_signal()
 
+                # Darkness sensor:
+                darkness = self.darkness_sensor.is_darkness()
+
+                # Light management:
+                new_light = darkness and presence
+                if not is_light_on and new_light:
+                    # Turn on the light immediately, if darkness and presence
+                    is_light_on = True
+                    floating_time = None
+
+                elif is_light_on == new_light:
+                    floating_time = None
+
+                else:
+                    # The last case - the light is on but can be turned off
+                    # but not immediately
+                    if not floating_time:
+                        floating_time = time_ms()
+
+                    else:
+                        if time_ms() - floating_time > self.MAX_FLOATING_TIME:
+                            is_light_on = False
+                            floating_time = None
+
+                # Finally make the physical light turn (or not):
+                self.light_switch.set_signal(is_light_on)
+
+                # Send current state to MQTT:
+                # self.mqtt.ping()
+                worker_data.data["darkness"] = darkness
+                worker_data.data["presence"] = presence
                 worker_data.data["radar"] = {
                     "presence": worker_data.data["presence"],
                     "target_state": data[0][0],
@@ -90,36 +123,15 @@ class KitchenWorker(Worker):
                     },
                     "distance": data[0][5]
                 }
+                worker_data.data["light"] = is_light_on
                 worker_data.data["time"] = time.localtime()
 
-                new_values = {"darkness": worker_data.data["darkness"],
-                    "presence": worker_data.data["presence"],
-                    "target_state": worker_data.data["radar"]["target_state"]}
-
-                if new_values != self.previous_values:
-                    # At least one of the three parameters has changed
-                    # Manage the light
-
-                    # TODO: should we also take a target_state into consideration?
-                    light = worker_data.data["darkness"] and worker_data.data["presence"]
-
-                    worker_data.data["light"] = light
-                    self.light_switch.set_signal(light)
-                    self.previous_values = new_values
-
-                # Testing for now. Instead of ping send data every second:
-                # self.mqtt.ping()
                 self.mqtt.publish(worker_data.data)
                 worker_data.mqtt_connected = self.mqtt.connected
-
                 time.sleep(worker_data.loop_sleep)
 
             except BaseException as e:
-                traceback = uio.StringIO()
-                sys.print_exception(e, traceback)
-                worker_data.error = traceback.getvalue()
-                worker_data.is_alive = False
-                worker_data.go_exit = True
+                self.handle_exception(e)
                 self.mqtt.publish_error(worker_data.error)
 
         try:
