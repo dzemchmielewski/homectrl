8aa95146e2e3e3025de9114556d00a63296e3645
Pawel Chmielewski <pawel.chmielewski@tylertech.com>
2024-10-15 23:36:50 +0200

    dev worker with bmp & aht - testings

--- board/dev_worker.py
+++ board/dev_worker.py
@@ -1,9 +1,10 @@
-import time
 from collections import deque
 
 from board.worker import MQTTWorker
+from modules.bmp_aht import BMP_AHT
 from modules.darkness import DarknessSensor
 from modules.pin_io import PinIO
+from common.common import time_ms
 
 
 class DevWorker(MQTTWorker):
@@ -11,11 +12,16 @@ class DevWorker(MQTTWorker):
     def __init__(self, debug=False):
         super().__init__("dev", debug)
         self.darkness = DarknessSensor.from_analog_pin(4)
+        self.reader = BMP_AHT.from_pins(2, 1)
+
         self.led = PinIO("LED", 0)
         self.led.set_signal(0)
         worker_data = self.get_data()
         worker_data.data = {
             "name": self.name,
+            "temperature": None,
+            "pressure": None,
+            "humidity":  None,
             "voltage": None,
             "datetime": None,
             "led": 0
@@ -38,6 +44,7 @@ class DevWorker(MQTTWorker):
     def start(self):
         self.begin()
         worker_data = self.get_data()
+        previous_sensor_read_time = None
         queue = deque((), 5)
         count = 0
 
@@ -61,6 +68,15 @@ class DevWorker(MQTTWorker):
                     worker_data.data["led"] = (worker_data.data["led"] + 1) % 2
                     self.led.set_signal(worker_data.data["led"])
                     count = 0
+
+                # BMP & AHT sensor:
+                if previous_sensor_read_time is None or time_ms() - previous_sensor_read_time > (60 * 1_000):
+                    readings = (self.reader.temperature, self.reader.pressure, self.reader.humidity)
+                    if readings != (worker_data.data["temperature"], worker_data.data["pressure"], worker_data.data["humidity"]):
+                        publish = True
+                        (worker_data.data["temperature"], worker_data.data["pressure"], worker_data.data["humidity"]) = readings
+                    worker_data.data["read_sensor"] = self.the_time_str()
+                    previous_sensor_read_time = time_ms()
 
                 if publish:
                     self.mqtt_publish()
