e5d1a37fa015c1895ee49880040cf32e8ea5c35c
Pawel Chmielewski <pawel.chmielewski@tylertech.com>
2024-11-06 00:16:55 +0100

    Socket worker implementation

--- board/socket_worker.py
+++ board/socket_worker.py
@@ -1,43 +1,29 @@
+from collections import deque
+
 from board.worker import MQTTWorker
 from modules.darkness import DarknessSensor
-from modules.pin_io import PinIO
+from modules.pinio import PinIO
 from common.common import time_ms
 
 
 class SocketWorker(MQTTWorker):
 
-    class StatusIndicator:
-        def __init__(self, touch: PinIO, red: PinIO):
-            self.showTime = None
-            self.touch = touch
-            self.red = red
-
-        def handle(self):
-            if self.touch.get_signal() == 1:
-                self.showTime = time_ms()
-            elif self.showTime is not None and time_ms() - self.showTime > 5 * 1_000:
-                self.showTime = None
-
-            self.red.set_signal(1 if self.showTime else 0)
-
     def __init__(self, debug=False):
         super().__init__("socket", debug)
-        # self.darkness = DarknessSensor.from_analog_pin(4)
+        self.darkness = DarknessSensor.from_analog_pin(4, queue_size=1, voltage_threshold=2.31)
+        self.relay = PinIO(0, 0)
+        self.status_indicator = SocketWorker.StatusIndicator(5, 6, 7, 8)
 
-        self.relay = PinIO("RELAY", 0)
-        self.touch = PinIO("TOUCH", 4)
-        self.relay.set_signal(0)
         worker_data = self.get_data()
         worker_data.loop_sleep = 0.2
         worker_data.data = {
-            "relay": None
+            "relay": None,
+            "process": None,
+            "darkness": None,
+            "debug_voltage": None
         }
         worker_data.control = {
             "mode": "auto"
-        }
-        self.status_indicator = SocketWorker.StatusIndicator(self.touch, self.relay)
-        worker_data.debug = {
-            "showTime": self.status_indicator.showTime
         }
 
     def capabilities(self):
@@ -48,7 +34,7 @@ class SocketWorker(MQTTWorker):
                     "type": "str",
                     "constraints": {
                         "type": "enum",
-                        "values": ["on", "off", "auto"]
+                        "values": ["on", "auto", "off"]
                     }
                 }
             ]}
@@ -61,10 +47,16 @@ class SocketWorker(MQTTWorker):
             try:
                 publish = False
 
-                if worker_data.control["mode"] == "auto":
-                    # TODO: implement auto mode
-                    relay = worker_data.data["relay"] if worker_data.data["relay"] is not None else 0
+                # Read darkness:
+                darkness, mean_voltage, voltage = self.darkness.read_analog()
+                worker_data.data["debug_voltage"] = (mean_voltage, voltage)
+                if darkness != worker_data.data["darkness"]:
+                    worker_data.data["darkness"] = darkness
+                    publish = True
 
+                # Set relay:
+                if worker_data.control["mode"] == "auto":
+                    relay = darkness
                 elif worker_data.control["mode"] == "on":
                     relay = 1
                 elif worker_data.control["mode"] == "off":
@@ -76,11 +68,14 @@ class SocketWorker(MQTTWorker):
                 if relay != worker_data.data["relay"]:
                     publish = True
                     worker_data.data["relay"] = relay
-                    self.relay.set_signal(relay)
-                worker_data.data["read"] = self.the_time_str()
+                    self.relay.set(relay)
 
-                self.status_indicator.handle()
-                worker_data.debug["showTime"] = self.status_indicator.showTime
+                # Handle lights indicators:
+                self.status_indicator.refresh(worker_data.control["mode"], relay)
+
+                # Save last process readable time
+                worker_data.data["process"] = self.the_time_str()
+
                 if publish:
                     self.mqtt_publish()
                 else:
@@ -90,3 +85,34 @@ class SocketWorker(MQTTWorker):
                 self.handle_exception(e)
 
         self.end()
+
+    class StatusIndicator:
+        def __init__(self, touch: int, blue: int, red: int, green: int):
+            self.showTime = time_ms()
+            self.touch = PinIO(touch)
+            self.red = PinIO(red, False)
+            self.green = PinIO(green, False)
+            self.blue = PinIO(blue, False)
+
+        def refresh(self, mode: str, relay: int):
+            if self.touch.get() == 1:
+                self.showTime = time_ms()
+            elif self.showTime is not None and time_ms() - self.showTime > 5 * 1_000:
+                self.showTime = None
+
+            if self.showTime is not None:
+                self.red.off()
+                self.green.off()
+                self.blue.off()
+            else:
+                if mode == "auto":
+                    self.blue.toggle()
+                else:
+                    self.blue.on()
+                if relay:
+                    self.green.on()
+                    self.red.off()
+                else:
+                    self.green.off()
+                    self.red.on()
+
