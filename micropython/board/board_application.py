a32e51bd9fb38572a563efc996e79dab5d968477
Pawel Chmielewski <pawel.chmielewski@tylertech.com>
2025-02-20 22:47:23 +0100

    Added 'control' functionality back to the new async code

--- micropython/board/board_application.py
+++ micropython/board/board_application.py
@@ -5,21 +5,22 @@ import os
 import board.board_shared as shared
 from board.board_web_socket import BoardWebSocket
 from board.mqtt_as import MQTTClient
+from board.mqtt_as import config as mqttconfig
+from configuration import Configuration
 
 from board.boot import Boot
 import json, machine, esp32, gc, ubinascii
 
-class MQTT:
+class MQTT(shared.Exitable, shared.Named):
 
     I_AM_ALIVE = json.dumps({"live": True, 'message': 'hello'})
     LAST_WILL = json.dumps({"live": False, 'message': "last will"})
     END_OF_LIVE = json.dumps({"live": False, 'message': "goodbye"})
 
-    def __init__(self, name: str):
-        from board.mqtt_as import config as mqttconfig
-        from configuration import Configuration
-        (self.topic_live, _, _, _, self.topic_control) = Configuration.topics(name)
-        # shared.EventConsumer.__init__(self, 'mqtt')
+    def __init__(self, app_name):
+        shared.Exitable.__init__(self)
+        shared.Named.__init__(self, 'mqtt')
+        (self.topic_live, _, _, _, self.topic_control) = Configuration.topics(app_name)
         mqttconfig['server'] = Configuration.MQTT_SERVER
         mqttconfig['user'] = Configuration.MQTT_USERNAME
         mqttconfig['password'] = Configuration.MQTT_PASSWORD
@@ -32,7 +33,6 @@ class MQTT:
         MQTTClient.DEBUG = False
         self.client = MQTTClient(mqttconfig)
         self._up_task = asyncio.create_task(self.on_up())
-        self._messages_task = asyncio.create_task(self.messages())
 
     async def connect(self):
         try:
@@ -40,11 +40,6 @@ class MQTT:
         except OSError as e:
             self.log.error(f"MQTT connecting FAILED")
             self.log.exception(e)
-
-    async def messages(self):
-        async for topic, msg, retained in self.client.queue:
-            self.log.info(f"MQTT incoming - topic: '{topic.decode()}', message: '{msg.decode()}', retained: {retained}")
-
 
     async def on_up(self):
         while not self.exit:
@@ -54,34 +49,27 @@ class MQTT:
             await self.client.publish(self.topic_live, self.I_AM_ALIVE, True)
             await self.client.subscribe(self.topic_control)
             self.log.info(f"MQTT listen to topic: {self.topic_control}")
-    #
-    # async def consume(self, event: dict):
-    #     self.log.info(f"MQTT event: {event}")
-    #     topic = event.get('topic')
-    #     data = event.get('data')
-    #     if topic is None or data is None:
-    #         await self.client.publish(self.topic_live, json.dumps({"live": True, "error": f"Cannot publish - topic or data is empty: {topic}, {data}"}))
-    #     else:
-    #         await self.client.publish(topic, json.dumps(data))
 
     def deinit(self):
-        self.log.info("DEINIT")
         self.client.publish(self.topic_live, self.END_OF_LIVE, True)
         super().deinit()
         self._up_task.cancel()
-        self._messages_task.cancel()
         self.client.disconnect()
 
 
-class BoardApplication(shared.Named):
+class BoardApplication(shared.Named, shared.Exitable):
 
     def __init__(self, name: str):
         shared.Exit.init()
         if not hasattr(self, 'name'):
             shared.Named.__init__(self, name)
+        shared.Exitable.__init__(self)
         self.start_time = time.ticks_ms()
         self.mqtt = None
         self.ws_server = BoardWebSocket({'self': self} | globals())
+        (self.topic_live, _, self.topic_state, self.topic_capabilities, _) = Configuration.topics(name)
+        self.control = {}
+        self.capabilities = {'controls': []}
 
     def info(self):
         return json.dumps({
@@ -104,13 +92,35 @@ class BoardApplication(shared.Named):
     async def start(self):
         self.mqtt = MQTT(self.name)
         await self.mqtt.connect()
+        await self.publish(self.topic_state, self.control, True)
+        await self.publish(self.topic_capabilities, self.capabilities, True)
+
+    async def mqtt_messages(self):
+        async for topic, msg, retained in self.mqtt.client.queue:
+            message = msg.decode()
+            self.log.info(f"MQTT incoming - topic: '{topic.decode()}', message: '{message}', retained: {retained}")
+            try:
+                controls = json.loads(message)
+                for key, value in self.validate_controls(self.capabilities, controls).items():
+                    self.control[key] = value
+                await self.publish(self.topic_state, self.control, True)
+            except Exception as e:
+                await self.publish(self.topic_live,
+                             {"live": True, 'error': f"Incoming MQTT message problem: {message}, error: {e}"}, False)
 
     async def _asyncio_entry(self):
         asyncio.create_task(self.start())
+        self._mqtt_messages_task = asyncio.create_task(self.mqtt_messages())
         asyncio.create_task(self.ws_server.start_server())
         while not shared.Exit.is_exit():
             await asyncio.sleep(1)
+
+    def go_exit(self):
+        shared.Exit.go()
+
+    def deinit(self):
         self.ws_server.shutdown()
+        self._mqtt_messages_task.cancel()
 
     def run(self):
         async def nothing():
@@ -135,6 +145,33 @@ class BoardApplication(shared.Named):
         result = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
         if days:
             result = "{:d} days ".format(days) + " " + result
+        return result
+
+    @staticmethod
+    def validate_controls(capabilities, input: dict) -> dict:
+        result = {}
+        if capabilities := capabilities:
+
+            for control in capabilities["controls"]:
+                name = control["name"]
+                constraints = control["constraints"]
+
+                if name in input:
+                    value = input[name]
+
+                    # Check type using eval on control_type
+                    if not isinstance(value, eval(control["type"])):
+                        continue  # Invalid type, skip this entry
+
+                    # Validate constraints
+                    if constraints["type"] == "enum":
+                        if value in constraints["values"]:
+                            result[name] = value  # Valid enum value
+
+                    elif constraints["type"] == "range":
+                        if constraints["values"]["min"] <= value <= constraints["values"]["max"]:
+                            result[name] = value  # Valid range value
+
         return result
 
 # if __name__ == "__main__":
