e50df12fa4983d5f28862e51295fcc8edcd27a93
Pawel Chmielewski <pawel.chmielewski@tylertech.com>
2025-09-26 23:56:20 +0200

    [frontend] Controls nicer look

--- frontend/src/components/Controls.js
+++ frontend/src/components/Controls.js
@@ -39,7 +39,7 @@ const Controls = (props) => {
             console.log('WebSocket connection established.');
         };
         socket.onmessage = (event) => {
-            //console.log(event.data)
+            // console.log(event.data)
             const receivedMessage = JSON.parse(event.data);
             setState(receivedMessage.result);
         };
@@ -65,51 +65,56 @@ const Controls = (props) => {
         let deviceState = state.find(s => s.name === name)
         if (!deviceState) { deviceState = {}}
 
-        return <ul key={"ctrl_" + name} className={"list-group"}>
+        return <div>
             {controls.map((c, ctrl_index) => {
                 if (["str"].includes(c.type)){
                     if (c.constraints.type === "enum") {
                         const current_value = deviceState[c.name]
                         return <li key={ctrl_index} className={"list-group-item ps-0"}>
                             <div className="d-flex flex-row align-items-center">
-                                <span className={"m-3"}>{c.name}</span>
-                                <div className="btn-group" role="group">
-                                    {c.constraints.values.map((value, val_index) => {
-                                        return <>
-                                            <input type="radio" className="btn-check"
-                                                   // autoComplete="off"
-                                                   name={"ctrl_" + name + "_control_" + ctrl_index}
-                                                   id={"ctrl_" + name + "_control_" + ctrl_index + "_option_" + val_index}
-                                                   checked={current_value === value}
-                                                   onChange={(e) => sendControl(e, name, c.name, value)}
-                                            />
-                                            <label className="btn btn-outline-primary" htmlFor={"ctrl_" + name + "_control_" + ctrl_index + "_option_" + val_index}>{value}</label>
-                                        </>
-                                    })}
+                                <div>
+                                    <button type="button" className="btn btn-outline-warning disabled" style={{width: '100px'}}>{c.name}</button>
+                                </div>
+                                <div className="d-flex flex-grow-1 justify-content-center">
+                                    <div className="btn-group" role="group">
+                                        {c.constraints.values.map((value, val_index) => {
+                                            return <>
+                                                <input type="radio" className="btn-check"
+                                                       // autoComplete="off"
+                                                       name={"ctrl_" + name + "_control_" + ctrl_index}
+                                                       id={"ctrl_" + name + "_control_" + ctrl_index + "_option_" + val_index}
+                                                       checked={current_value === value}
+                                                       onChange={(e) => sendControl(e, name, c.name, value)}
+                                                />
+                                                <label className="btn btn-outline-primary" htmlFor={"ctrl_" + name + "_control_" + ctrl_index + "_option_" + val_index}>{value}</label>
+                                            </>
+                                        })}
+                                    </div>
                                 </div>
                             </div>
 
                         </li>
                     }
                 }
-                // return <li key={ctrl_index}>
-                //     <span>{c.name}</span> <small>not available</small>
-                // </li>
             })}
-        </ul>
+        </div>
     }
 
     function renderDeviceControls() {
         return Object.keys(capabilities).map(name => {
-            if (!IsAlive(name, deviceContext)) {
+            if (!IsAlive(name, deviceContext) || !capabilities[name].controls || capabilities[name].controls.length === 0) {
                 return null;
             }
             return (
-                <li key={name}
-                    className="list-group-item d-flex justify-content-around align-items-center ps-1">
-                    <div>{name}</div>
-                    {show_controls(name, capabilities[name].controls)}
-                </li>
+                <div className="card border-secondary mb-3">
+                    <div className="card-header" >{name}</div>
+                    <div className="card-body">
+                        <span className="card-text">
+                            {show_controls(name, capabilities[name].controls)}
+                        </span>
+                    </div>
+                </div>
+
             );
         }).filter(Boolean);
     }
@@ -125,9 +130,7 @@ const Controls = (props) => {
             {isExpanded(props.facet) && (
             <div className="card-body">
                 <span className="card-text">
-                    <ul className="list-group">
                         {renderDeviceControls()}
-                    </ul>
                 </span>
             </div>
             )}
