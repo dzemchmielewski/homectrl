76ee61ac7619bb387d81916c5be5a1353ccaabe9
Pawel Chmielewski <pawel.chmielewski@tylertech.com>
2025-11-06 23:29:58 +0100

    [devices] Refactoring - move files to 'devices'

--- devel/esp32_setup.py
+++ devel/esp32_setup.py
@@ -87,9 +87,9 @@ class Esp32Setup:
 
         rshell = create_temp_file("rshell.py")
         with open(rshell, "w") as f:
-            f.write("cp {} {}/\n".format(os.path.join(src_dir, "esp32/configuration.py"), self.dest_board))
+            f.write("cp {} {}/\n".format(os.path.join(src_dir, "devices/configuration.py"), self.dest_board))
             f.write("cp {} {}/\n".format(os.path.join(src_dir, "secrets.json"), self.dest_board))
-            f.write("cp {} {}/\n".format(os.path.join(src_dir, "esp32/reboot.py"), self.dest_board))
+            f.write("cp {} {}/\n".format(os.path.join(src_dir, "devices/reboot.py"), self.dest_board))
             if self.worker_name:
                 f.write("cp {} {}/{}\n".format(main, self.dest_board, self.dest_main))
                 f.write("cp {} {}/\n".format(os.path.join(src_dir, "devices/{}.py".format(worker_module)), self.dest_board))
